c70bb5efaebf66eb99fe3029b70566a4f5177e91
Sally Steuterman <sally.steuterman@gmail.com>
2019-09-16 14:05:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Write your JavaScript code here!
+
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+<h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${}</li>
+   <li>Diameter: ${}</li>
+   <li>Star: ${}</li>
+   <li>Distance from Earth: ${}</li>
+   <li>Number of Moons: ${}</li>
+</ol>
+<img src="${data.image}">
+*/
